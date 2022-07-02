0bb726b4d2091f33b76c95a991e0725fdfee9ab2
Velin-Todorov <velintodorov@yahoo.com>
2022-07-02 22:36:51 +0300

    Encapsulation Lab ex4

---
+++ Softuni Python OOP/Encapsulation/Encapsulation Lab/Email Validator.py
@@ -0,0 +1,23 @@
+class EmailValidator:
+    def __init__(self, min_length, mails, domains):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name):
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail):
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain):
+        return domain in self.domains
+
+    def validate(self, email):
+        name, mail_domain = email.split('@')
+        mail, domain = mail_domain.split('.')
+
+        if self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain):
+            return True
+
+        return False
