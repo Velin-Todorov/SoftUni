35cac307b233d01970d3d4c1ec110bb19cdb983f
Velin-Todorov <velintodorov@yahoo.com>
2022-07-03 20:04:53 +0300

    Encapsulation Tutorial ex2

--- Softuni Python OOP/Encapsulation/Encapsulation Tutorial/Pizza Maker/project/pizza.py
+++ Softuni Python OOP/Encapsulation/Encapsulation Tutorial/Pizza Maker/project/pizza.py
@@ -9,6 +9,36 @@ class Pizza:
         self.topping_capacity = topping_capacity
         self.toppings = {}
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError('The name cannot be an empty string')
+        self.__name = value
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        if value is None:
+            raise ValueError('You should add dough to the pizza')
+        self.__dough = value
+
+    @property
+    def topping_capacity(self):
+        return self.__topping_capacity
+
+    @topping_capacity.setter
+    def topping_capacity(self, value):
+        if value <= 0:
+            raise ValueError("The topping's capacity cannot be less or equal to zero")
+        self.__topping_capacity = value
+
     def add_topping(self, topping: Topping):
 
         if len(self.toppings) >= self.topping_capacity:
@@ -27,8 +57,3 @@ class Pizza:
         pizza_weight = toppings_weight + self.dough.weight
 
         return pizza_weight
-
-
-
-
-
