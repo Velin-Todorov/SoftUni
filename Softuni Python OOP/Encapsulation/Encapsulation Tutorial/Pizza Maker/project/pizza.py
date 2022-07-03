56070c9bfc2bb7f4f5c17bf8856681790e63f66d
Velin-Todorov <velintodorov@yahoo.com>
2022-07-03 20:15:25 +0300

    Encapsulation Tutorial ex2

--- Softuni Python OOP/Encapsulation/Encapsulation Tutorial/Pizza Maker/project/pizza.py
+++ Softuni Python OOP/Encapsulation/Encapsulation Tutorial/Pizza Maker/project/pizza.py
@@ -3,10 +3,10 @@ from project.topping import Topping
 
 
 class Pizza:
-    def __init__(self, name: str, dough: Dough, topping_capacity: int):
+    def __init__(self, name: str, dough: Dough, toppings_capacity: int):
         self.name = name
         self.dough = dough
-        self.topping_capacity = topping_capacity
+        self.toppings_capacity = toppings_capacity
         self.toppings = {}
 
     @property
@@ -30,24 +30,24 @@ class Pizza:
         self.__dough = value
 
     @property
-    def topping_capacity(self):
-        return self.__topping_capacity
+    def toppings_capacity(self):
+        return self.__toppings_capacity
 
-    @topping_capacity.setter
-    def topping_capacity(self, value):
+    @toppings_capacity.setter
+    def toppings_capacity(self, value):
         if value <= 0:
             raise ValueError("The topping's capacity cannot be less or equal to zero")
-        self.__topping_capacity = value
+        self.__toppings_capacity = value
 
     def add_topping(self, topping: Topping):
 
-        if len(self.toppings) >= self.topping_capacity:
+        if len(self.toppings) >= self.toppings_capacity:
             raise ValueError('Not enough space for another topping')
 
-        if topping in self.toppings.keys():
-            self.toppings[topping] += topping.weight
-
-        self.toppings[topping] = topping.weight
+        if topping.topping_type in self.toppings:
+            self.toppings[topping.topping_type] += topping.weight
+        else:
+            self.toppings[topping.topping_type] = topping.weight
 
     def calculate_total_weight(self):
         toppings_weight = 0
