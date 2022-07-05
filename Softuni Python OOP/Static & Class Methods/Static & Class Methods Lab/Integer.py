cb1cdf956f41c7c94be3c28c779d1beb943768b8
Velin-Todorov <velintodorov@yahoo.com>
2022-07-05 09:28:51 +0300

    Static & Class methods ex3

---
+++ Softuni Python OOP/Static & Class Methods/Static & Class Methods Lab/Integer.py
@@ -0,0 +1,40 @@
+import math
+
+
+class Integer:
+    def __init__(self, value: int):
+        self.value = value
+
+    @classmethod
+    def from_float(cls, float_value):
+        if type(float_value) != float:
+            return 'value is not a float'
+        float_value = math.floor(float_value)
+        return cls(float_value)
+
+    @classmethod
+    def from_roman(cls, value):
+
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40,
+                 'XC': 90, 'CD': 400, 'CM': 900}
+        i = 0
+        num = 0
+        while i < len(value):
+            if i + 1 < len(value) and value[i:i + 2] in roman:
+                num += roman[value[i:i + 2]]
+                i += 2
+            else:
+                # print(i)
+                num += roman[value[i]]
+                i += 1
+        value = num
+        return cls(value)
+
+    @classmethod
+    def from_string(cls, value):
+        if type(value) != str:
+            return 'wrong type'
+        value = int(value)
+        return cls(value)
+
+
