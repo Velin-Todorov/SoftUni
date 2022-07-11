4f1ac077798f62834d94de7adc003a7d87e1a467
Velin-Todorov <velin729@gmail.com>
2022-07-11 21:08:39 +0200

    Recursion Lab ex.3

---
+++ Algorithms with Python/Recursion/Recursion Lab/Generating 0 1 vectors.py
@@ -0,0 +1,16 @@
+def generating_vectors(idx, vector):
+
+    if idx > len(vector) - 1:
+        print(*vector)
+        return
+
+    for number in range(0, 2):
+        vector[idx] = number
+        generating_vectors(idx + 1, vector)
+
+
+
+n = int(input())
+vector = [None] * n
+
+generating_vectors(0, vector)
