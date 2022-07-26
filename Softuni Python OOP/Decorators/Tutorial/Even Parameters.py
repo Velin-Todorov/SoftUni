4da3ca19a24fca0217f37023234a72b26f21bae8
Velin-Todorov <velintodorov@yahoo.com>
2022-07-26 20:46:09 +0200

    Decorators Tutorial ex.2

---
+++ Softuni Python OOP/Decorators/Tutorial/Even Parameters.py
@@ -0,0 +1,29 @@
+import functools
+
+def even_parameters(func):
+
+    @functools.wraps(func)
+    def wrapper(*args):
+        try:
+            even_numbers = [x for x in args if x % 2 == 0]
+
+            if len(even_numbers) == len(args):
+                result = func(*args)
+                return result
+            else:
+                return 'Please use only even numbers!'
+        except:
+            return 'Please use only even numbers!'
+
+    return wrapper
+
+
+@even_parameters
+def multiply(*nums):
+    result = 1
+    for num in nums:
+        result *= num
+    return result
+
+print(multiply(2, 4, 6, 8))
+print(multiply(2, 4, 9, 8))
