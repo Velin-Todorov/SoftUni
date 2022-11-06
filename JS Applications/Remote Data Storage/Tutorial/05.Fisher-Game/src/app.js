4faf0983a9dd0c74f4b0c9b2fae74e63f4319116
Velin - Todorov <velintodorov@yahoo.com>
2022-11-06 19:56:08 +0100

    JS Applications

--- JS Applications/Remote Data Storage/Tutorial/05.Fisher-Game/src/app.js
+++ JS Applications/Remote Data Storage/Tutorial/05.Fisher-Game/src/app.js
@@ -1,1 +1,63 @@
-console.log('TODO:// Implement Home functionality');
+window.addEventListener('DOMContentLoaded', onLoadHTML)
+
+document.getElementById('logout').addEventListener('click', onLogout);
+
+async function onLogout() {
+    const url = "http://localhost:3030/users/logout";
+    
+    const header = getHeader("GET", '')
+    const response = await fetch(url, header)
+    sessionStorage.clear();
+    onLoadHTML();
+
+}
+
+function onLoadHTML(){
+
+    const token = sessionStorage.getItem('accessToken')
+    const greeting = document.querySelector('p.email span')
+
+    if (token){
+        document.getElementById('guest').style.display = 'none'    
+        document.getElementById('user').style.display = 'inline-block'
+        greeting.innerHTML = sessionStorage.getItem('email')
+        
+    }else{
+        document.getElementById('guest').style.display = 'inline-block'
+        document.getElementById('user').style.display = 'none'
+        greeting.innerHTML = 'guest'
+
+    }
+}
+
+async function onCreateCatch(body){
+    const url = 'http://localhost:3030/data/catches'
+    const header = getHeader("POST", body)
+    const response = await fetch(url, header)
+    const data = await response.json();
+
+    return data
+}
+
+async function onLoadCatch(body, header){
+    const url = 'http://localhost:3030/data/catches'
+
+}
+
+
+function getHeader(method, body){
+
+    const token = sessionStorage.getItem('accessToken')
+
+    const header = {
+        method: `${method}`,
+        headers: {
+            "Content-Type": "application/json",
+            "X-Authorization" : token
+        },
+        if (body){
+            header.body= JSON.stringify(body)
+        }
+    }
+    return header
+}
--- JS Applications/Remote Data Storage/Tutorial/05.Fisher-Game/src/register.js
+++ JS Applications/Remote Data Storage/Tutorial/05.Fisher-Game/src/register.js
@@ -1,1 +1,72 @@
-console.log('TODO:// Implement Register functionality');
+document.getElementById('register-form').addEventListener('submit', registration)
+document.querySelectorAll('a').forEach(x => x.classList.remove('active'))
+document.getElementById('register').classList.add('active')
+document.getElementById('user').style.display = 'none'
+
+const errorP = document.querySelector('p.notification');
+
+
+function registration(ev) {
+    ev.preventDefault();
+
+    const form = ev.target
+
+    const formData = new FormData(form)
+    const { email, password, rePass } = Object.fromEntries(formData);
+
+    if (password !== rePass) {
+        errorP.textContent = 'Passwords do not match!'
+        setTimeout(() => {
+            errorP.textContent = ''
+        }, 1000)
+    }else{
+        onRegister(email, password)
+
+    }
+
+
+
+}
+
+async function onRegister(email, password) {
+
+    const url = 'http://localhost:3030/users/register';
+
+    const body = {
+        email,
+        password
+    }
+
+    const header = getHeader("POST", body);
+
+    try {
+        const response = await fetch(url, header)
+        const data = await response.json()
+        if (data.code !== 200 && data.code){
+            throw new Error(data.message)
+        }
+        sessionStorage.setItem('email', data.email);
+        sessionStorage.setItem('accessToken', data.accessToken);
+
+        window.location = './index.html'
+
+        return data
+    }catch (e){
+        errorP.textContent = e
+        setTimeout(() => {
+            errorP.textContent = ''
+        }, 1000)
+    }
+    
+}
+
+
+function getHeader(method, body) {
+    return {
+        method: `${method}`,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+}
