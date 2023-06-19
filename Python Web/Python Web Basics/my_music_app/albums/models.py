a22c0fe4302cd479cc1d7225639c60d8b0e39573
Velin-Todorov <velintodorov@yahoo.com>
2023-06-19 19:40:54 +0200

    Created models

--- Python Web/Python Web Basics/my_music_app/albums/models.py
+++ Python Web/Python Web Basics/my_music_app/albums/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
 # Create your models here.
+class Album:
+    album_name = models.CharField(
+        unique=True, 
+        max_length=30, blank=False, 
+        null=False
+    )
+
+    artist = models.CharField(
+        max_length=30, 
+        blank=False, 
+        null=False
+    )
+
+    genre = models.CharField(
+        max_length=30, 
+        blank=False, 
+        null=False,
+        choices=[
+            ('pop_music', 'Pop Music'),
+            ('jazz', 'Jazz Music'),
+            ('r&b', 'R&B Music'),
+            ('rock', 'Rock Music'),
+            ('country', 'Country Music'),
+            ('dance', "Dance Music"),
+            ('hipHop', 'Hip Hop Music'),
+            ('other', 'Other')
+        ]
+    )
+    description = models.TextField(blank=True, null=True)
+    image_url = models.URLField(blank=False, null=False)
+    price = models.FloatField(
+        blank=False, 
+        null=False, 
+        validators=[MinValueValidator(0.0)])
--- Python Web/Python Web Basics/my_music_app/user_profile/models.py
+++ Python Web/Python Web Basics/my_music_app/user_profile/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.core.validators import MinLengthValidator, MaxLengthValidator, Mi
+from validators import validate_username
 # Create your models here.
+
+class Profile:
+    username = models.CharField(
+        validators= [
+            MinLengthValidator(
+                limit_value=2,
+                message="Username cannot be shorter than 2 characters"
+            ),
+            MaxLengthValidator(
+                limit_value=15,
+                message="Username cannot be longer than 15 characters"
+            ),
+            validate_username
+        ]
+    )
+    email = models.EmailField(
+        blank=False, 
+        null=False
+    )
+    
+    age = models.PositiveIntegerField(
+        blank=True, 
+        null=True,
+    )
