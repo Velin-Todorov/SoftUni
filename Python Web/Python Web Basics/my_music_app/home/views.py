9f44eec473c25ee9dedbcd6e5821466dc47b15d5
Velin-Todorov <velintodorov@yahoo.com>
2023-06-19 22:01:47 +0200

    implemented home page

--- Python Web/Python Web Basics/my_music_app/home/views.py
+++ Python Web/Python Web Basics/my_music_app/home/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home_page(request):
+    user = request.user
+
+    if not user.is_authenticated:
+        return render(
+            request,
+            'home/home-no-profile.html',
+            {'user': user}
+        )
+    
--- Python Web/Python Web Basics/my_music_app/my_music_app/settings.py
+++ Python Web/Python Web Basics/my_music_app/my_music_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static/',
+)
+
+STATIC_ROOT = '/my_music_app/static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
