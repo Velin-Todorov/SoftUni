f38025cfebf4b31fe81fb73144cac81d558faaa9
Velin-Todorov <velintodorov@yahoo.com>
2023-06-23 23:37:14 +0200

    Finished models implementations

--- Python Web/Python Web Basics/my_music_app/albums/models.py
+++ Python Web/Python Web Basics/my_music_app/albums/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from auth_user.models import Profile
 
 # Create your models here.
-class Album:
+class Album(models.Model):
 
     GENRES =(
         ('pop', 'Pop Music'),
@@ -19,6 +20,7 @@ class Album:
     album_name = models.CharField(unique=True, max_length=30)
     artist = models.CharField(max_length=30, blank=False, null=False)
     genre = models.CharField(
+        max_length=15,
         blank=False,
         null=False,
         choices = GENRES
@@ -27,3 +29,4 @@ class Album:
     description = models.TextField(blank=True, null=True)
     image_url = models.URLField(blank=False, null=False)
     price =  models.FloatField(blank=False, null=False, validators=[MinValueValidator(0)])
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
--- Python Web/Python Web Basics/my_music_app/auth_user/models.py
+++ Python Web/Python Web Basics/my_music_app/auth_user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MinValueValidator, RegexValidator
 
+
 # Create your models here.
 class Profile(models.Model):
     username = models.CharField(
@@ -11,7 +12,7 @@ class Profile(models.Model):
                 'The username must be a minimum of 2 symbols'
             ),
             RegexValidator(
-                '\w+',
+                '\w+$',
                 'Ensure this value contains only letters, numbers, and underscore.'
             )
         ]
