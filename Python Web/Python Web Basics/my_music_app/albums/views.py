2e6ac42c3dffb0da2933c53836e33d914184fb3d
Velin-Todorov <velintodorov@yahoo.com>
2023-06-23 23:38:19 +0200

    finished views

--- Python Web/Python Web Basics/my_music_app/albums/views.py
+++ Python Web/Python Web Basics/my_music_app/albums/views.py
@@ -1,17 +1,69 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import AlbumForm
+from auth_user.models import Profile
+from .models import Album
 
 # Create your views here.
 def add_album(request):
-    return render(request)
+    profile = Profile.objects.first()
 
-def add_album(request):
-    return render(request)
+    form = AlbumForm(request.POST or None)
 
-def edit_album(request):
-    return render(request)
+    if form.is_valid():
+        album = form.save(commit=False)
+        album.profile = profile
+        album.save()
+        return redirect('home_page')
 
-def delete_album(request):
-    return render(request)
+    context = {
+        'form': form,
+        'profile': profile  
+    }
 
-def details_album(request):
-    return render(request)
+
+    return render(request, 'add-album.html', context)
+
+
+def edit_album(request, id):
+    album = Album.objects.get(pk=id)
+    
+    if request.method == 'GET':
+   
+        form = AlbumForm(instance=album)
+        context = {'form': form}
+        return render(request, 'edit-album.html', context)
+
+    elif request.method == 'POST':
+        form = AlbumForm(request.POST, instance=album)
+        context = {'form': form}
+        
+        if form.is_valid():
+            print('form is valid')
+            form.save()
+            return redirect('home_page')
+        
+        else:
+            print('form is invalid')
+            return render(request, 'edit-album.html', context)
+
+def delete_album(request, id):
+
+    album = Album.objects.get(pk=id)
+    
+    if request.method == 'GET':
+        form = AlbumForm(instance=album)
+        context = {'form': form}
+        return render(request, 'delete-album.html', context)
+    
+    if request.method == 'POST':
+        album.delete()
+        return redirect('home_page')
+        
+    
+
+def details_album(request, id):
+    album = Album.objects.get(pk=id)
+    
+    context = {'album': album}
+    return render(request, 'album-details.html', context)
--- Python Web/Python Web Basics/my_music_app/auth_user/views.py
+++ Python Web/Python Web Basics/my_music_app/auth_user/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Profile
+from albums.models import Album
 # Create your views here.
 
-def create_profile(request):
-    return render(request)
-
 def delete_profile(request):
-    return render(request)
+    profile = Profile.objects.first()
+    profile.delete()
+    return redirect('home_page')
 
 def details_profile(request):
-    return render(request)
+    profile = Profile.objects.first()
+    albums = Album.objects.filter(profile_id=profile.pk)
+    
+    context = {
+        'profile': profile,
+        'album_count': len(albums)
+    }
+    return render(request, 'profile-details.html', context)
