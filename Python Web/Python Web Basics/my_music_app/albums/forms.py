3481ae20f836a1d54d28adfd6208a93d6035677e
Velin-Todorov <velintodorov@yahoo.com>
2023-06-24 00:16:06 +0200

    Implemented a deleted form

--- Python Web/Python Web Basics/my_music_app/albums/forms.py
+++ Python Web/Python Web Basics/my_music_app/albums/forms.py
@@ -28,3 +28,11 @@ class AlbumForm(forms.ModelForm):
         }
         exclude = ['profile']
 
+class DeleteAlbumForm(AlbumForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for (_, field) in self.fields.items():
+            field.widget.attrs['disabled'] = 'disabled'
+            field.widget.attrs['readonly'] = 'readonly'
+
