59a9b94b23490cc5967441982bc5f12ec714e928
Velin-Todorov <velintodorov@yahoo.com>
2023-06-23 23:50:26 +0200

    finished views for common app

--- Python Web/Python Web Basics/my_music_app/common/views.py
+++ Python Web/Python Web Basics/my_music_app/common/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from auth_user.models import Profile
+from auth_user.forms import ProfileForm
+from albums.models import Album
 
 # Create your views here.
 def home_page(request):
     profile = Profile.objects.first()
-    context = {'profile': profile}
+    albums = Album.objects.all()
+    context = {}
 
-    return render(
-        request,
-        'home_custom.html',
-        context
-    )
+    print(albums)
+    if not profile:
+        form = ProfileForm(request.POST or None)
+        context['form'] = form
+
+        if form.is_valid():
+            form.save()
+            print('Form is valid')
+            return redirect(request.path_info)
+            
+        
+        return render(
+                request,
+                'home-no-profile.html',
+                context
+            )
+
+
+    context = {'albums': albums}
+    return render(request, 'home-with-profile.html', context)
 
