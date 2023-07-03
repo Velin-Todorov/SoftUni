816c3107d656cb7cc708fe4ab9d3d4459bd16e47
Velin-Todorov <velintodorov@yahoo.com>
2023-07-03 22:33:44 +0200

    used .env file

--- Python Web/Python Web Framework/kvartiri_bg/kvartiri_bg/settings.py
+++ Python Web/Python Web Framework/kvartiri_bg/kvartiri_bg/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from environ import
+from dotenv import get_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i$=)_hqqniu3g_5c-z2ppccv0c#indh%awf@%#bgi4)^aeg(83'
+SECRET_KEY = get_key('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
